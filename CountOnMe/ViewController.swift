d17d0e1c94c96fb3d42d592861c4580ba4de4acc
Mohammed El Hamdi <elhamdi56@gmail.com>
2023-01-11 16:25:56 +0000

    fix:add_fonctinnalities( +,=,-,*,/,%)

--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -1,50 +1,182 @@
 
 import UIKit
 
-class ViewController: UIViewController, CalcInteractor {
-    private lazy var calculator : CalcModel = CalcModel(interactor: self)
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet var numberButtons: [UIButton]!
+class ViewController: UIViewController{
     
-    func onError(message: String) {
-        let alertVC = UIAlertController(title: "Zéro!", message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-        self.present(alertVC, animated: true, completion: nil)
-    }
     
-    func onText(text: String) {
-        textView.text = text
-    }
+    @IBOutlet weak var calculatorWorkings: UILabel!
     
-
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    var workings : String = ""
+    
     // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        clearAll()
         // Do any additional setup after loading the view.
     }
+    func clearAll() {
+        workings = ""
+        calculatorWorkings.text = ""
+        calculatorResults.text = ""
+    }
     
+    @IBAction func equalsTap(_ sender: Any) {
+        if(validInput())
+                {
+                    let checkedWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*0.01")
+                    let expression = NSExpression(format: checkedWorkingsForPercent)
+                    let result = expression.expressionValue(with: nil, context: nil) as! Double
+                    let resultString = formatResult(result: result)
+                    calculatorResults.text = resultString
+                }
+                else
+                {
+                    let alert = UIAlertController(
+                        title: "Invalid Input",
+                        message: "Calculator unable to do math based on input",
+                        preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Okay", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                }
+        
+    }
     
-    // View actions
-    @IBAction func tappedNumberButton(_ sender: UIButton) {
-        guard let numberText = sender.title(for: .normal) else {
-            return
+    func validInput() ->Bool
+        {
+            var count = 0
+            var funcCharIndexes = [Int]()
+            
+            for char in workings
+            {
+                if(specialCharacter(char: char))
+                {
+                    funcCharIndexes.append(count)
+                }
+                count += 1
+            }
+            
+            var previous: Int = -1
+            
+            for index in funcCharIndexes
+            {
+                if(index == 0)
+                {
+                    return false
+                }
+                
+                if(index == workings.count - 1)
+                {
+                    return false
+                }
+                
+                if (previous != -1)
+                {
+                    if(index - previous == 1)
+                    {
+                        return false
+                    }
+                }
+                previous = index
+            }
+            
+            return true
         }
         
-        self.calculator.tapped(number: numberText)
+        func specialCharacter (char: Character) -> Bool
+        {
+            if(char == "*")
+            {
+                return true
+            }
+            if(char == "/")
+            {
+                return true
+            }
+            if(char == "+")
+            {
+                return true
+            }
+            return false
+        }
+    @IBAction func AllClearTap(_ sender: Any) {
+        clearAll()
     }
     
-    @IBAction func tappedAdditionButton(_ sender: UIButton) {
-        self.calculator.tappedOpe(operand: "+")
+    @IBAction func backTap(_ sender: Any) {
+        if(!workings.isEmpty){
+            workings.removeLast()
+            calculatorWorkings.text = workings
+        }
+    }
+    func addToWorkings(value : String){
+        workings = workings + value
+        calculatorWorkings.text = workings
+    }
+    @IBAction func percentTap(_ sender: Any) {
+        addToWorkings(value: "%")
+    }
+    @IBAction func divideTap(_ sender: Any) {
+        addToWorkings(value: "/")    }
+    
+    @IBAction func timesTamp(_ sender: Any) {
+        addToWorkings(value: "*")    }
+    
+    @IBAction func minusTap(_ sender: Any) {
+        addToWorkings(value: "-")    }
+    
+    @IBAction func plusTap(_ sender: Any) {
+        addToWorkings(value: "+")    }
+    
+    @IBAction func decimalTap(_ sender: Any) {
+        addToWorkings(value: ".")    }
+    
+    func formatResult(result : Double) -> String {
+        if(result.truncatingRemainder(dividingBy: 1) == 0)
+                {
+                    return String(format: "%.0f", result)
+                }
+                else
+                {
+                    return String(format: "%.2f", result)
+                }
     }
     
-    @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-        self.calculator.tappedOpe(operand: "-")
-        
-    }
-
-    @IBAction func tappedEqualButton(_ sender: UIButton) {
-        self.calculator.makeOperation()
-    }
-
+    @IBAction func zeroTap(_ sender: Any) {
+        addToWorkings(value: "0")    }
+    
+    @IBAction func sevenTap(_ sender: Any) {
+        addToWorkings(value: "7")    }
+    
+    @IBAction func heightTap(_ sender: Any) {
+        addToWorkings(value: "8")    }
+    
+    @IBAction func nineTap(_ sender: Any) {
+        addToWorkings(value: "9")    }
+    
+    @IBAction func sixTap(_ sender: Any) {
+        addToWorkings(value: "6")    }
+    
+    @IBAction func fiveTap(_ sender: Any) {
+        addToWorkings(value: "5")    }
+    
+    @IBAction func fourTap(_ sender: Any) {
+        addToWorkings(value: "4")    }
+    
+    @IBAction func threeTap(_ sender: Any) {
+        addToWorkings(value: "3")    }
+    
+    @IBAction func twoTap(_ sender: Any) {
+        addToWorkings(value: "2")    }
+    
+    @IBAction func oneTap(_ sender: Any) {
+        addToWorkings(value: "1")     }
 }
+
+   
+    
+
+    
+   
 
